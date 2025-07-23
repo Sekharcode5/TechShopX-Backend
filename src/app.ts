a43355f36de6b841a79a31102a68f957f8d1afe9
Sekharcode5 <vadlapudisekhar987@gmail.com>
2025-07-24 00:25:57 +0530

    fix

--- src/app.ts
+++ src/app.ts
@@ -41,7 +41,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
